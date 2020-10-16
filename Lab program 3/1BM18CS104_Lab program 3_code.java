6ad4c75e134c7fad154f510af8b9098ac0cbdbe3
Shreyas K S <54268899+SHREYAS-KS-CS104@users.noreply.github.com>
2020-10-16 12:48:02 +0530

    Add files via upload

---
+++ Lab program 3/1BM18CS104_Lab program 3_code.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class book{
+String booktitle;
+String author;
+int no_of_pages;
+double price;
+Scanner sc = new Scanner(System.in);
+book(){
+System.out.print("Enter book title:");
+booktitle = sc.nextLine();
+System.out.print("Enter author name:");
+author = sc.nextLine();
+System.out.print("Enter price:");
+price = sc.nextDouble();
+System.out.print("Enter no of pages:");
+no_of_pages = sc.nextInt();
+}
+public String toString(){
+return("Book name ="+booktitle+" Author ="+author+ "Price ="+price+" Pages ="+no_of_pages);
+}
+}
+class Books{
+public static void main(String[] args){
+int n,i;
+Scanner in = new Scanner(System.in);
+System.out.print("Enter number of books ");
+n = in.nextInt();
+book[] b = new book[n];
+for(i = 0;i < n;i++)
+{
+System.out.println("Enter details of Book "+(i+1));
+b[i] = new book();
+}
+for(i = 0;i < n;i++)
+{
+System.out.println(b[i]);
+}
+}
+}
